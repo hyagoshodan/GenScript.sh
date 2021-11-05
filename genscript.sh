1b1e0e64380217f85ec444745322bf18002d1ca3
hyagoshodan <hyagodejesus@outlook.com>
2021-11-05 12:22:32 -0300

    Primeira versão pronta!

--- genscript.sh
+++ genscript.sh
@@ -2,15 +2,53 @@
 # genscript.sh version 1.0.0
 
 ################################### Sobre ###################################
-# genscript.sh
+# O genscript.sh é um programa simples que gera um Shell-Script com um modelo -
+# padrão com informações passadas pelo usuário. 
+# Escrito em 03/11/2021 por Hyago de Jesus.
+# Este software é distribuido pela licença MIT e não dá nenhuma garantia.
+
+################################# Parâmetros #################################
+NOME="$1"   # Guardando o primeiro parâmetro dentro da variável "NOME"
+
+################################### Checks ###################################
+# Realizando teste para verificar se o arquivo já existe
+if [ -f $NOME ]
+then
+    read -p "O arquivo já existe, gostaria de sobreescrever? [S\n]: " OPCAO
+    
+        case $OPCAO in 
+            S | s)
+                echo "Continuando..."
+                ;;
+            N | n)
+                read -p "Escolha outro nome: " NOME
+                ;;
+            *)
+                echo "Opção incorreta."
+                
+        esac
+
+fi
+
+################################### Funções ##################################
+touch $NOME     # Criando o arquivo  
+
+echo "#!/bin/bash
+# $NOME version 1.0.0
+
+################################### Sobre ###################################
 
 
 ################################# Parâmetros #################################
 
-################################## Variáveis #################################
+ 
+################################# Variáveis ##################################
+
+
+################################### Checks ###################################
+
 
 ################################### Funções ##################################
 
-################################### Checks ##################################
 
-#################################### Logs ###################################
+#################################### Logs ####################################" > "$NOME"
